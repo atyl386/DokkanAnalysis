b67bc800c1ea8e4c2e721506c8d9c1300d0ba1d2
AJ Tyler <atyl386@aucklanduni.ac.nz>
2023-11-12 12:22:47 +1300

    Deleted old files

--- createKitTemplates.py
+++
@@ -1,7 +0,0 @@
-import shutil
-src = r'C:\Users\Tyler\Documents\DokkanAnalysis\DokkanKitTemplate.xlsx'
-dir = r'C:\Users\Tyler\Documents\DokkanAnalysis\DokkanKits'
-for i in range(112,200):
-    filename = str(i)+".xlsx"
-    dst = dir+"\\"+filename
-    shutil.copyfile(src,dst)
