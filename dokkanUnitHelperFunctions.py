dfa05fc620f60acad47b83dd045ba4750003f522
AJ Tyler <atyl386@aucklanduni.ac.nz>
2023-11-18 17:36:47 +1300

    KiModifier bug fix

--- dokkanUnitHelperFunctions.py
+++ dokkanUnitHelperFunctions.py
@@ -25,7 +25,9 @@ def SAMultiplier(baseMultiplier, nCopies, nStacks, saAtk):
 
 def KiModifier(base, ki):
     """Returns the ki modifier for a unit"""
-    if ki <= 12:
+    if ki < 12:
+        return 1
+    elif ki == 12:
         return base
     else:
         return np.linspace(base, 2, 13)[ki - 12]
