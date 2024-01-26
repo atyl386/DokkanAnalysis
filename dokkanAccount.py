134755cd6bef8934712936907d7c39387270284c
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-01-27 12:33:56 +1300

    Added forgotten ability to universe 2

--- dokkanAccount.py
+++ dokkanAccount.py
@@ -185,6 +185,14 @@ User = {
         "HiPo Choice # 2": "DGE",
         "Slots": SLOT_1,
     },
+    24: {
+        "Common Name": "LR_TEQ_Universe_2",
+        "# Copies": 0,
+        "BRZ Equip": "DEF",
+        "HiPo Choice # 1": "ADD",
+        "HiPo Choice # 2": "DGE",
+        "Slots": SLOT_1,
+    },
 
 }
 """ ['DEF','ADD','DGE',3], # PHYLR SS4s
