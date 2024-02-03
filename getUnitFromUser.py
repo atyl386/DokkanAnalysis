01229263b4f35d227e9f1f0bcf6c8665b866a37c
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-02-03 14:06:57 +1300

    Add RoD Blues

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -2345,4 +2345,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(37, "DFLR_TEQ_Orange_Piccolo", 1, "DEF", "DGE", "ADD", SLOT_1)
+    unit = Unit(38, "DF_INT_RoF_Blues", 1, "DEF", "DGE", "ADD", SLOT_1)
