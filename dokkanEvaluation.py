25db45cde0d7afe9b11fc2997f86c96011da1c99
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-02-05 23:05:18 +1300

    Decreased leader skill again

--- dokkanEvaluation.py
+++ dokkanEvaluation.py
@@ -106,15 +106,15 @@ overallTurnWeights = turnDistribution.cdf(np.arange(2, NUM_EVAL_TURNS + 2)) - tu
     "Slot1": 4, # 0.09
 }"""
 attributeDict = {
-    "Leader Skill": 4.5, # -
+    "Leader Skill": 4, # 0
     "SBR": 0.5,
-    "HP": 2, # 1.5
-    "Useability": 4, # 3.5
-    "Healing": 1.5,
-    "Support": 5, # 4.5
+    "HP": 2, # +
+    "Useability": 4, # +
+    "Healing": 1.5, # 0
+    "Support": 5, # 0
     "APT": 10,
-    "Normal Defence": 8, # 9
-    "Super Attack Defence": 8, # 9
+    "Normal Defence": 8, # +
+    "Super Attack Defence": 8, # +
     "Slot Bonus": 1
 }
 overallEvaluator = Evaluator(overallTurnWeights, attributeDict.values())
