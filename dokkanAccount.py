2d1ca88aa9bbdc187f68701107f2e65b10c3fe10
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-02-24 13:29:13 +1300

    Add Evo Blue vegeta still not done!

--- dokkanAccount.py
+++ dokkanAccount.py
@@ -720,5 +720,13 @@ User = {
         "HiPo Choice # 1": "ADD",
         "HiPo Choice # 2": "DGE",
         "Slots": SLOT_1,
+    },
+    91: {
+        "Common Name": "DFLR_INT_EvoBlue_Vegeta",
+        "# Copies": 5,
+        "BRZ Equip": "DEF",
+        "HiPo Choice # 1": "ADD",
+        "HiPo Choice # 2": "DGE",
+        "Slots": SLOT_1,
     }
 }
