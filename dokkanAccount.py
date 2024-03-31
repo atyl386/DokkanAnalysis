0281425f7c13947814617109ff097a14ffeff280
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-03-31 15:19:30 +1300

    Add STR Nappa

--- dokkanAccount.py
+++ dokkanAccount.py
@@ -1057,4 +1057,12 @@ User = {
         "HiPo Choice # 2": "DGE",
         "Slots": SLOT_2,
     },
+    133: {
+        "Common Name": "BU_STR_Nappa_",
+        "# Copies": 5,
+        "BRZ Equip": "DEF",
+        "HiPo Choice # 1": "DGE",
+        "HiPo Choice # 2": "ADD",
+        "Slots": SLOT_2,
+    },
 }
