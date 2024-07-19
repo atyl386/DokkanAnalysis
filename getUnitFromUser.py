a9ac04fbd0ab10e7f6ad22210ecf74aee8f8ef8b
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-07-19 21:39:08 +1200

    Add Bardock and Gine

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -2952,4 +2952,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(222, "LR_PHY_Android17_Frieza", 5, "ATK", "ADD", "DGE", SLOT_2)
+    unit = Unit(223, "BU_STR_Bardock_Gine", 5, "DEF", "DGE", "ADD", SLOT_2)
