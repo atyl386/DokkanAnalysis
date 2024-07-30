1fb3ef430111fcc469a7a28197e3a9549e3cf810
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-07-30 21:51:07 +1200

    Add Bunny Bulma

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -2960,4 +2960,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(231, "LR_TEQ_Full_Power_Jiren", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(232, "BU_AGL_Bulma_Bunny", 5, "DEF", "DGE", "ADD", SLOT_2)
