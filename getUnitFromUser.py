dd8e6115b98ee6b34b2711b1a0ee6fe7f5c0c094
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-08-17 15:40:24 +1200

    Add TEQ Mai

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -2970,4 +2970,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(240, "BU_INT_Cocotte", 5, "DGE", "DGE", "ADD", SLOT_2)
+    unit = Unit(241, "BU_TEQ_Mai", 5, "DGE", "DGE", "ADD", SLOT_2)
