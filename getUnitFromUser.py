1d85648cfba07aa3e4534dc7d8a38c37bc5e5658
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-08-20 22:18:34 +1200

    Add Team Turles EZA

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -2970,4 +2970,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(241, "BU_TEQ_Mai", 5, "DGE", "DGE", "ADD", SLOT_2)
+    unit = Unit(242, "LR_STR_Team_Turles", 5, "DEF", "DGE", "ADD", SLOT_2)
