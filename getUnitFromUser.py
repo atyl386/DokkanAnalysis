7080f21f0a9168caced112a9cefa6fda5395e43d
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-10-22 21:48:44 +1300

    Add F2P Gohan & Piccolo

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3076,4 +3076,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(272, "F2PLR_PHY_Version_Z_Goku", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(274, "F2P_AGL_Gohan_Piccolo", 5, "DEF", "DGE", "ADD", SLOT_2)
