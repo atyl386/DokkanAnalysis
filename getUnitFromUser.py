799cca76a8451358678845b02447fc69b229aef0
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-10-23 21:57:49 +1300

    Add F2P STR Yamcha

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3076,4 +3076,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(274, "F2P_INT_Krillin", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(275, "F2P_STR_Yamcha", 5, "DEF", "DGE", "ADD", SLOT_2)
