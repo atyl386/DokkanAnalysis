9be248c9a5917d9e6b917f8d5a69e79ecd3cb087
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-10-26 09:15:12 +1300

    Add F2P PHY Chiaotzu

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3076,4 +3076,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(276, "F2P_TEQ_Tien", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(277, "F2P_PHY_Chiaotzu", 5, "DEF", "DGE", "ADD", SLOT_2)
