9b6b9a47eeac536ef6d6223b2d971033a5e47b56
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-11-10 23:26:36 +1300

    Add Fu Dogidogi

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3103,4 +3103,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(295, "BU_INT_Aeos", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(296, "BU_AGL_Fu_Dogidogi", 5, "DEF", "DGE", "ADD", SLOT_2)
