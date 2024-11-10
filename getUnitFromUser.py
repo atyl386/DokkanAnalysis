9e117a3afb3c3d1beecc969086683e4aa0c70222
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-11-11 00:31:13 +1300

    Add TEQ Beerus

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3103,4 +3103,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(297, "BU_STR_SSGSS_Goku_Universe_Tree", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(298, "DF_TEQ_Beerus", 5, "DEF", "DGE", "ADD", SLOT_2)
