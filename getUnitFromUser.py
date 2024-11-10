a6bd1baaa3640d711895a931888cea3940c25ed8
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-11-10 22:19:42 +1300

    Add Warrior in Black

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3103,4 +3103,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(292, "F2P_STR_Towa", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(293, "BU_PHY_Warrior_In_Black", 5, "DEF", "DGE", "ADD", SLOT_2)
