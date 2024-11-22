d2e1bf3bea0ce7256560f074708f92a588bfd0d7
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-11-23 10:34:34 +1300

    Add STR F2P Champa

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3101,4 +3101,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(301, "F2P_TEQ_Saonel_Pirina", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(302, "F2P_STR_Champa", 5, "DEF", "DGE", "ADD", SLOT_2)
