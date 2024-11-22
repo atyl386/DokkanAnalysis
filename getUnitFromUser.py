4905642de0ff82022010688830d4ae8eab6ad9ac
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-11-23 09:57:19 +1300

    Add F2P Frieza and Cooler

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3101,4 +3101,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(298, "DF_TEQ_Beerus", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(299, "F2P_PHY_Frieza_Cooler", 5, "DEF", "DGE", "ADD", SLOT_2)
