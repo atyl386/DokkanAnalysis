f6bfd1696c1ef9280e5d8232b685a4e22b6b25e9
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-11-23 10:14:24 +1300

    Add F2P PHY Hit

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3101,4 +3101,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(299, "F2P_PHY_Frieza_Cooler", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(300, "F2P_PHY_Hit", 5, "DEF", "DGE", "ADD", SLOT_2)
