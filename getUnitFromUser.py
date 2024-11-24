046fee5b69141f04dafb0f52673744ec0a8ce9c6
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-11-25 09:57:33 +1300

    Add TEQ ToP Android 17

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3101,4 +3101,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(303, "BU_INT_Piccolo_ToP", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(304, "BU_TEQ_ToP_Android_17", 5, "DEF", "DGE", "ADD", SLOT_2)
