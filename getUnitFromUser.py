fa8e18dd2a6facc248858bf1e63c15ff4ceb6b6c
AJ Tyler <atyl386@aucklanduni.ac.nz>
2024-12-07 08:22:07 +1300

    Add DF TEQ majin vegeta

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3114,4 +3114,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(311, "DF_INT_SS3_Angel_Goku", 5, "DEF", "DGE", "ADD", SLOT_2)
+    unit = Unit(312, "DF_TEQ_Majin_Vegeta", 5, "DEF", "DGE", "ADD", SLOT_2)
