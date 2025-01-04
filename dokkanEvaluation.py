04271284207037e2a0d22963873b503158b87719
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-01-05 10:57:38 +1300

    Only use evalUnitIdxs

--- dokkanEvaluation.py
+++ dokkanEvaluation.py
@@ -211,7 +211,7 @@ if __name__ == "__main__":
             output = np.asarray(output, dtype=object)
         units[-1] = np.array(list(output[:, 0]))
         attributeValues[:, :, :, -1] = list(output[:, 1])
-        [rainbowMeans, rainbowStds] = summaryStats(attributeValues[:, :, :, -1])
+        [rainbowMeans, rainbowStds] = summaryStats(attributeValues[evalUnitIdxs, :, :, -1])
         for ID in reverseOrderIDs:
             normalizeUnit(units[-1][ID - 1], rainbowMeans, rainbowStds)
             evaluations[ID - 1, -1] = overallEvaluator.evaluate(units[-1][ID - 1])
