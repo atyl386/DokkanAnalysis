f7ea9344d4d2bf99c4187ff50ea759962d17dbfe
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-01-05 14:39:06 +1300

    Add comment

--- dokkanSummoning.py
+++ dokkanSummoning.py
@@ -127,7 +127,7 @@ class Banner:
         self.summonScore = self.units * self.coin * self.featuredRate * self.tickets * self.threePlus1 * discount * self.anniBonus
 
     def shouldSummmon(self):
-        if self.summonScore > 10:  # Will need to be tuned
+        if self.summonScore > 10:  # Will need to be tuned. Could be as high as 15
             return True
         else:
             return False
