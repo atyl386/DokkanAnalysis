815bfa0647d820fb3e54e2ebfe3db4c861799d24
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-01-05 14:47:51 +1300

    Add comment

--- dokkanUnitConstants.py
+++ dokkanUnitConstants.py
@@ -119,7 +119,7 @@ PEAK_TURN = 7  # Most important turn
 NUM_COPIES_MAX = 5
 AVG_PEAK_TURN = 3.75744
 PEAK_TURN_STD = 1.857813
-MIN_EVALUATION = -3 # Corresponds to unit being just useable
+MIN_EVALUATION = -3 # Corresponds to unit being just useable when rainbowed
 ATTTRIBUTE_NAMES = [
     "Leader Skill",
     "SBR",
