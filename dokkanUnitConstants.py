0a74802a8c8a8ef6c6c177447ca9e97f780f29d2
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-01-13 21:19:01 +1300

    Update to Goku & Frieza's actual damage

--- dokkanUnitConstants.py
+++ dokkanUnitConstants.py
@@ -277,7 +277,7 @@ AVG_DAM_VARIANCE = 1.015
 MAX_T1_NORMAL_DAM = 1001000  # SSGSSE Vegeta Supreme Battle Spectacle
 MAX_NORMAL_DAM = 1900000  # Goku & Frieza Supreme Battle Spectacle (950000 * 2)
 MAX_T1_SA_DAM = 2541000  # SSGSSK Goku & SSGSSE Vegeta (770000 * (3 + 0.3))
-MAX_SA_DAM = 3800000  # Goku & Frieza Supreme Battle Spectacle (950000 * (3 + 1))
+MAX_SA_DAM = 5700000  # Goku & Frieza Supreme Battle Spectacle (2850000 * (1 + 1))
 MAX_NORMAL_DAM_PER_TURN = (
     LOOK_AHEAD_FACTOR
     * AVG_DAM_VARIANCE
