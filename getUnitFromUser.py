1f548d8c898f9e4b495e182a04534830eb2123e0
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-01-25 12:05:03 +1300

    Add new TODO

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -6,6 +6,7 @@ import click as clc
 import glob
 
 # TODO:
+# - Maybe should make model non-linear by having a survivability term that is multiplied by the remaining terms, e.g. DPT, support, leader skill, etc.
 # - Make nullification a regular Buff so can do per turn like STR Kid Buu
 # - Make better way to integrate the no eval unit finding into normal evaluation run
 # - Simplify getEventFactor code
