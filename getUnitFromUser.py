d5a3e24438571b8ed38eb720a49f05b1a4722b1e
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-02-16 01:20:20 +1300

    Add Banner unit AGL Beerus

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3973,4 +3973,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(333, "LR_INT_SS2_Gohan", 5, "ATK", "ADD", "DGE", [1, 1, 1, 2, 2, 2, 2, 2, 2, 2], "True")
+    unit = Unit(334, "BU_AGL_Beerus", 5, "ATK", "ADD", "DGE", [1, 1, 1, 2, 2, 2, 2, 2, 2, 2], "True")
