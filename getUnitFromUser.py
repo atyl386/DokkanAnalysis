01017ba257d185e689c176b7a2c1cf5b56023adb
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-02-16 11:55:48 +1300

    Add CLR SS2 Gohan

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3973,4 +3973,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(335, "CLR_INT_Goku_Frieza", 5, "ATK", "ADD", "DGE", [1, 1, 1, 2, 2, 2, 2, 2, 2, 2], "True")
+    unit = Unit(336, "CLR_PHY_SS2_Gohan", 5, "ATK", "ADD", "DGE", [1, 1, 1, 2, 2, 2, 2, 2, 2, 2], "True")
