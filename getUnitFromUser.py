ab7d532d3d401f3487085a63a59583fb7108426e
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-02-21 20:57:03 +1300

    Add Exchange Goten & Trunks

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3973,4 +3973,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(337, "DFLR_AGL_Super_Gogeta", 5, "ATK", "ADD", "DGE", [1, 1, 1, 2, 2, 2, 2, 2, 2, 2], "True")
+    unit = Unit(338, "F2P_PHY_Trunks_Goten", 5, "ATK", "ADD", "DGE", [1, 1, 1, 2, 2, 2, 2, 2, 2, 2], "True")
