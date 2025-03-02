1068eb43ccfe1dffffa93d1739fa45a613e28625
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-03-02 15:14:03 +1300

    OPtimise

--- dokkanUnitConstants.py
+++ dokkanUnitConstants.py
@@ -1323,5 +1323,5 @@ HIPO_SPECIAL_EQUIPS = {
 }
 
 FIXED_SLOT_UNITS = {
-    332: "DF TEQ Pefect Cell", # Slot 1 so can get the orbs the model thinks he can
+    #332: "DF TEQ Pefect Cell", # Slot 1 so can get the orbs the model thinks he can
 }
