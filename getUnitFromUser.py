ef2740be8900a6ed4211ee8a80c02ac4fc9c3180
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-03-03 18:59:26 +1300

    Add SEZA SS3 Vegeta

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -3973,4 +3973,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(58, "DFLR_TEQ_God_Goku_Vegeta", 5, "ATK", "ADD", "DGE", [1, 1, 1, 2, 2, 2, 2, 2, 2, 2], "True")
+    unit = Unit(341, "DF_PHY_SS_Goku_GT", 5, "ATK", "ADD", "DGE", [1, 1, 1, 2, 2, 2, 2, 2, 2, 2], "True")
