5861c4cda69e2ce2c003fedb5dd0822112910f1c
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-04-10 08:13:35 +1200

    Add Paikuhan and Janemba EZAs

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -4095,4 +4095,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(354, "BU_STR_Paikuhan", 5, "DEF", "ADD", "DGE", [2, 3, 3, 3, 3, 3, 3, 3, 3, 3], "True")
+    unit = Unit(48, "DF_AGL_Paikuhan", 5, "DEF", "ADD", "DGE", [2, 3, 3, 3, 3, 3, 3, 3, 3, 3], "True")
