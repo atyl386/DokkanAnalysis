bc614177d690cf4d4af11e012de806f4253e860d
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-04-27 22:25:21 +1200

    Add SEZA AGL SSGSSK Goku

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -4101,4 +4101,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(359, "DF_TEQ_SSGSS_Kaioken_Goku", 5, "DEF", "ADD", "DGE", [1, 1, 1, 3, 3, 3, 3, 3, 3, 3], "True")
+    unit = Unit(360, "DF_AGL_SSGSS_Kaioken_Goku", 5, "DEF", "ADD", "DGE", [1, 1, 1, 3, 3, 3, 3, 3, 3, 3], "True")
