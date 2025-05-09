ceeaee9d499931ac974dfac1248db1f0211906c3
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-05-10 10:41:46 +1200

    Add F2P INT SS Future Gohan

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -4105,4 +4105,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(362, "F2PLR_STR_Mercenary_Tao", 5, "DEF", "ADD", "DGE", [1, 1, 1, 3, 3, 3, 3, 3, 3, 3], "True")
+    unit = Unit(363, "F2P_INT_SS_Future_Gohan", 5, "DEF", "ADD", "DGE", [1, 1, 1, 3, 3, 3, 3, 3, 3, 3], "True")
