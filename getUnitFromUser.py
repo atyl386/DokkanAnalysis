993bec562accfdf42ff181f89b2983dca1085d67
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-05-10 11:08:43 +1200

    Add PHY Boujack

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -4105,4 +4105,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(363, "F2P_INT_SS_Future_Gohan", 5, "DEF", "ADD", "DGE", [1, 1, 1, 3, 3, 3, 3, 3, 3, 3], "True")
+    unit = Unit(364, "F2P_PHY_Boujack", 5, "DEF", "ADD", "DGE", [1, 1, 1, 3, 3, 3, 3, 3, 3, 3], "True")
