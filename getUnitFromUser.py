99dbbde5d8bd4df43ac1e23d32a6e790e34da7df
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-05-28 20:58:56 +1200

    Add General Blue

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -4174,4 +4174,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(20, "DF_TEQ_Fusing_Kefla", 5, "ATK", "CRT", "ADD", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
+    unit = Unit(367, "BU_TEQ_General_Blue", 5, "ATK", "CRT", "ADD", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
