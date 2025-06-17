f5f8a0311ee27db24097e91f3a208d8e45cda261
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-06-17 21:36:44 +1200

    Up enemy DEF

--- dokkanUnitConstants.py
+++ dokkanUnitConstants.py
@@ -262,7 +262,7 @@ PROBABILITY_KILL_ENEMY_PER_ATTACK = (
 # Enemy
 LOOK_AHEAD_FACTOR = 1.2  # Want to know which units will be good in the future when enemies are even stronger
 AVG_ENEMY_DMG_RED = 0.7
-MAX_T1_ENEMY_DEF = 500000  # UI Goku Supreme Battle Spectacle
+MAX_T1_ENEMY_DEF = 1100000  # Goku (Youth) (Giant Ape) Red DB Red Zone
 MAX_ENEMY_DEF = 2000000 # Goku & Frieza SMB
 MAX_ENEMY_DEF_PER_TURN = (
     LOOK_AHEAD_FACTOR
