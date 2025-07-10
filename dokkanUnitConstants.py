db418f7e221b956b9182918f32cd7a00e22e3dd6
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-07-10 23:08:09 +1200

    New hardest fight

--- dokkanUnitConstants.py
+++ dokkanUnitConstants.py
@@ -285,10 +285,10 @@ MAX_ENEMY_DMG_THRESHOLD_PER_TURN = (
 ENEMY_DMG_THRESHOLD_CHANCE = 0.5 # 10th Anniv Final Red Zone Stages
 ENEMY_DODGE_CHANCE = 0.05
 AVG_DAM_VARIANCE = 1.015
-MAX_T1_NORMAL_DAM = 1687500  # TEQ Gogeta (450000 * (1 + 0.5) * (1 + 1.5))
-MAX_NORMAL_DAM = 3250000  # SMB Vegito Blue (2500000 * (1 + 0.3))
-MAX_T1_SA_DAM = 4500000  # TEQ Gogeta 450000 * (3.5 + 0.5) * (1 + 1.5)
-MAX_SA_DAM = 8250000  # SMB Vegito Blue (2500000 * (3 + 0.3))
+MAX_T1_NORMAL_DAM = 1687500  # Goku Black & Zamasu (1250000 * (1 + 0.5) * (1 + 0.5)) * 0.6 (Whis)
+MAX_NORMAL_DAM = 3375000  # # Goku Black & Zamasu (1250000 * (1 + 0.5) * (1 + (0.3 - 0.1) * 10))) * 0.6 (Whis)
+MAX_T1_SA_DAM = 4500000  # Goku Black & Zamasu 1250000 * (3.5 + 0.5) * (1 + 0.5) * 0.6 (Whis)
+MAX_SA_DAM = 9000000  # Goku Black & Zamasu 1250000 * (3.5 + 0.5) * (1 + (0.3 - 0.1) * 10))) * 0.6 (Whis)
 MAX_NORMAL_DAM_PER_TURN = (
     LOOK_AHEAD_FACTOR
     * AVG_DAM_VARIANCE
