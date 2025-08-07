aa9d68e10c6bda1dc2dec9c686044e4f0e95101f
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-08-07 22:30:48 +1200

    Add EX Part Units

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -4241,4 +4241,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(85, "DFLR_INT_Vegeta_Trunks", 5, "ATK", "CRT", "ADD", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
+    unit = Unit(401, "DFLR_INT_SS_Hybrid_Saiyans", 5, "ATK", "CRT", "ADD", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
