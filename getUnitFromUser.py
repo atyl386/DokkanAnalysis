456d8824f751cea93d2a02f9993c127c31807c58
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-09-15 22:01:48 +1200

    Add Broly EZAs

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -4231,4 +4231,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(410, "CLR_TEQ_Wrathful_Broly", 5, "ATK", "CRT", "ADD", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
+    unit = Unit(412, "LR_STR_LSS_Broly", 5, "ATK", "CRT", "ADD", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
