045b6c4e7f9e6ee14fa90745f9d87588140abe12
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-10-16 21:31:29 +1300

    Add Part 2 units

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -4231,4 +4231,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(415, "LR_TEQ_Super_17", 5, "ATK", "CRT", "ADD", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
+    unit = Unit(421, "F2PLR_STR_Chi_Chi", 5, "ATK", "CRT", "ADD", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
