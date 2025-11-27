953d468aa787f591fada0fa6c79f8cd2df387194
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-11-28 11:06:50 +1300

    Add Super Garlic Jr.

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -4258,4 +4258,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(437, "DF_AGL_Android_16", 5, "DGE", "DGE", "CRT", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
+    unit = Unit(438, "F2PLR_AGL_Garlic_Jr", 5, "DGE", "DGE", "CRT", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
