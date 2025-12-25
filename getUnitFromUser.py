3d3a5044d0b8a537aa5097b3323053ab6f0048c4
AJ Tyler <atyl386@aucklanduni.ac.nz>
2025-12-26 09:52:28 +1300

    Add initial Gohan and Gamma 1

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -4266,4 +4266,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(14, "LR_AGL_Future_Gohan_Trunks", 5, "DGE", "DGE", "CRT", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
+    unit = Unit(444, "DF_AGL_Gohan_Gamma1", 5, "DGE", "DGE", "CRT", [1, 1, 1, 1, 1, 1, 1, 1, 1, 2], "True")
