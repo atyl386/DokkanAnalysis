c0e84f4dda6af2eaad7c4684981ec027e62763c7
AJ Tyler <atyl386@aucklanduni.ac.nz>
2026-01-13 21:08:08 +1300

    Add comment about TODO for Ornage Piccolo's HiPo

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -6,6 +6,7 @@ import click as clc
 import glob
 
 # TODO:
+# - Respec Orange Piccolo's HiPo abilities
 # - For units that get important buffs next to a unit they will always be next to, should include those buffs in their kit
 # - Fix recievedOREvaded to use recievd and evaded attacks in atk calc
 # - Make better way to integrate the no eval unit finding into normal evaluation run
