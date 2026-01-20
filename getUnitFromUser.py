876b01605b938f83e648af5b0a8a0d17a2cb2a18
AJ Tyler <atyl386@aucklanduni.ac.nz>
2026-01-20 22:47:41 +1300

    round

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -539,7 +539,7 @@ class Unit:
             for i, state in enumerate(self.uninterpolatedStates):
                 outputFile.write(f"Form # {state.form.formIdx} / State # {i + 1} / Turn # {state.turn} \n \n")
                 for j, attributeName in enumerate(ATTTRIBUTE_NAMES):
-                    outputFile.write(f"{attributeName}: {self.uninterpolatedAttributes[i, j]} \n")
+                    outputFile.write(f"{attributeName}: {round(self.uninterpolatedAttributes[i, j], 6)} \n")
                 outputFile.write("\n")
 
 
