6df2f5e05d23dbf292a496c45467b361c23b69b6
AJ Tyler <atyl386@aucklanduni.ac.nz>
2026-01-31 01:25:58 +1300

    Optimise Anni Goku

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -6,7 +6,6 @@ import click as clc
 import glob
 
 # TODO:
-# - Allow for stacking supper supers like anni goku
 # - Respec Orange Piccolo's HiPo abilities
 # - For units that get important buffs next to a unit they will always be next to, should include those buffs in their kit
 # - Fix recievedOREvaded to use recievd and evaded attacks in atk calc
