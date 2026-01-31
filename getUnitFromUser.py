46dfb58879580734a66c4d1695908760ca843dc7
AJ Tyler <atyl386@aucklanduni.ac.nz>
2026-01-31 16:48:39 +1300

    Apply SS3 Vegeta DAIMA's support to other DAIMA units

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -6,7 +6,6 @@ import click as clc
 import glob
 
 # TODO:
-# Add extra dmg red, 20% def, guard and crit to daima units (on same 11 categories as ss3 vegeta and ss4 goku)
 # Fix Vegeta's EX Super. Should make a completely new type, other than 12 and 18 ki supers and remove hybrid colossal/mega-colossal multiplier
 # - For units that get important buffs next to a unit they will always be next to, should include those buffs in their kit
 # - Fix recievedOREvaded to use recievd and evaded attacks in atk calc
