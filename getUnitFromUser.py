4be5aa9d1e41c117bb0f1a46da484a96c6614de1
AJ Tyler <atyl386@aucklanduni.ac.nz>
2026-02-11 08:01:47 +1300

    Apply vegeta's support to tamagamis

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -6,7 +6,7 @@ import click as clc
 import glob
 
 # TODO:
-# Fix DAIMA SS4 Goku's crit in slot 2,3 from activating too late, shouldn't be after supering
+# Make sure links are all being added correctly to state
 # Make support section of input .xml if not used, uses the default support values
 # - For units that get important buffs next to a unit they will always be next to, should include those buffs in their kit
 # - Fix recievedOREvaded to use recievd and evaded attacks in atk calc
