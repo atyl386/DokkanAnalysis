c0425210718d9c4930f8b0c5e445434d71f2d293
AJ Tyler <atyl386@aucklanduni.ac.nz>
2026-02-15 20:32:57 +1300

    Fix bug

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -6,7 +6,6 @@ import click as clc
 import glob
 
 # TODO:
-# Add evasion to UI Goku EX Super
 # Make support section of input .xml if not used, uses the default support values
 # - For units that get important buffs next to a unit they will always be next to, should include those buffs in their kit
 # - Fix recievedOREvaded to use recievd and evaded attacks in atk calc
@@ -2236,7 +2235,7 @@ class State:
         """Returns the remaining damage taken by a unit in a turn recursively"""
         # Get damage taken by the attack pre super
         pEvadeB = (
-            self.multiChanceBuff["EvasionB"].chances["Start of Turn"]
+            self.multiChanceBuff["EvasionB"].chances["Start of Turn"] + self.multiChanceBuff["EvasionB"].chances["Super Attack Effect"]
             - self.multiChanceBuff["EvasionA"].chances["Start of Turn"]
         )
         dmgRedB = self.dmgRedNormalB - self.dmgRedNormalA
