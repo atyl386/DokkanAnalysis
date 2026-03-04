f8c9775b55653ba51f5176d24c7e75d02cda573b
AJ Tyler <atyl386@aucklanduni.ac.nz>
2026-03-04 21:32:48 +1300

    Add SEZA UI Goku

--- getUnitFromUser.py
+++ getUnitFromUser.py
@@ -4555,4 +4555,4 @@ class CompositeCondition:
 
 
 if __name__ == "__main__":
-    unit = Unit(3, "DFLR_STR_GT_Goku_Vegeta", 5, "DEF", "DGE", "ADD", [2, 2, 2, 3, 2, 2, 2, 2, 2, 2], "True")
+    unit = Unit(460, "DF_INT_UI_Goku", 5, "DEF", "DGE", "ADD", [2, 2, 2, 3, 2, 2, 2, 2, 2, 2], "True")
