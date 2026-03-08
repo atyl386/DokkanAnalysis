cfef2c317b5715ef54a9c4fbb0b9f542cd0cce01
AJ Tyler <atyl386@aucklanduni.ac.nz>
2026-03-08 16:06:32 +1300

    Relax useabiltiy weight

--- dokkanEvaluation.py
+++ dokkanEvaluation.py
@@ -8,7 +8,7 @@ import tqdm
 
 HIPO_DUPES = ["55%", "69%", "79%", "90%", "100%"]
 
-reCalc = False
+reCalc = True
 analyseHiPo = False
 optimiseslots = False
 accountRanking = True
@@ -256,7 +256,7 @@ if __name__ == "__main__":
         "Leader Skill": 2,
         "SBR": 1,
         "HP": 1.5,
-        "Useability": 10,
+        "Useability": 8,
         "Healing": 2,
         "Support": 6,
         "DPT No Look Ahead": 0,
